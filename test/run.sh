6fe2a576d7451cfb5260a231ebb72e42de1a98bf
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-02-13 06:14:28 +0530

    only push to dockerhub

--- test/run.sh
+++ test/run.sh
@@ -4,47 +4,48 @@ cd $(dirname $0)
 cd ../complete
 
 ./mvnw clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf target
 
-./mvnw clean install com.google.cloud.tools:jib-maven-plugin:dockerBuild
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
+# ./mvnw clean install com.google.cloud.tools:jib-maven-plugin:dockerBuild
+./mvnw com.google.cloud.tools:jib-maven-plugin:build -Dimage=puneetvashisht/gs-spring-boot-docker
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf target
 
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+# ./gradlew build
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf build
 
-./gradlew build jibDockerBuild
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+# ./gradlew build jibDockerBuild
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf build
 
-cd ../initial
+# cd ../initial
 
-./mvnw clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
+# ./mvnw clean compile
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf target
 
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
+# ./gradlew compileJava
+# ret=$?
+# if [ $ret -ne 0 ]; then
+# exit $ret
+# fi
+# rm -rf build
 
-exit
+# exit
